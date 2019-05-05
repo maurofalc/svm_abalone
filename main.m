668280716fb3168682440132c10897509adaa56b
Thales Rocha <thales_rochasousa@hotmail.com>
2019-05-04 23:15:24 -0300

    Adicionada predição da primeira amostra da base de dados para duas classes.

--- main.m
+++ main.m
@@ -1,10 +1,26 @@
 % Reconhecimento de Padrões
 % SVM - Abalone Dataset
 
-tbl = readtable('abalone.data','Filetype','text','ReadVariableNames',false);
+clear;
+clc;
+close all;
 
-mdl = fitrsvm(tbl,'Var9','KernelFunction','gaussian','KernelScale',2.2,'Standardize',true);
+tbl = readtable('abalone.data', 'Filetype', 'text', ...
+    'ReadVariableNames', false);
 
-CVMdl = crossval(mdl);
+% Faz com que todos da classe M e F virem classe MF
+for i = 1:height(tbl)
+    if tbl.Var1{i} == 'M' || tbl.Var1{i} == 'F'
+        tbl.Var1{i} = 'MF';
+    end
+end
 
-%openExample('stats/TrainSupportVectorMachineRegressionModelExample');
+mdl = fitcsvm(tbl(2:height(tbl), :), 'Var1', 'KernelFunction', ...
+    'gaussian', 'KernelScale', 2.2, 'Standardize', true);
+
+% Predição da primeira amostra
+[label, score] = predict(mdl, tbl(1, 2:9));
+fprintf("Classe predita: %s\n", string(label));
+
+% CVMdl = crossval(mdl);
+% openExample('stats/TrainSupportVectorMachineRegressionModelExample');
