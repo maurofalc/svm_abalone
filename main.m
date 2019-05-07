363f83116b6911ca4b739fb61be9421669e5aea8
Thales Rocha <thales_rochasousa@hotmail.com>
2019-05-06 22:43:48 -0300

    Adição do K-Fold como método avaliativo. Adição da matriz de confusão.

--- main.m
+++ main.m
@@ -1,33 +1,41 @@
-% Reconhecimento de Padrões
+%% Reconhecimento de Padrões
 % SVM - Abalone Dataset
 
 clear;
 clc;
 close all;
 
+%% Leitura da base de dados.
 tbl = readtable('abalone.data', 'Filetype', 'text', ...
-    'ReadVariableNames', false);
+    'ReadVariableNames', true);
 
+%% Separação dos dados.
 samples = tbl(:, 2:9);
-data1 = tbl(:, 1);
-data2 = tbl(:, 1);
-data3 = tbl(:, 1);
+classes = tbl(:, 1);
+data1 = classes;
+data2 = classes;
+data3 = classes;
+classes = cell2mat(table2array(classes));
 
-% Quantidade de amostras.
-N = height(samples);
+%% Parâmetros
+N = height(samples); % Quantidade de amostras.
+K = 10; % Tamanho do K-fold.
+kernel = 'rbf';
+scale = 2.2;
 
+%% Preparação das classes e dados
 for i = 1:N
     % Faz com que todos da classe M e F virem classe MF
-    if tbl.Var1{i} == 'M' || tbl.Var1{i} == 'F'
-        data1.Var1{i} = 'MF';
+    if tbl.Sex{i} == 'M' || tbl.Sex{i} == 'F'
+        data1.Sex{i} = 'MF';
     end
     % Faz com que todos da classe M e I virem classe MI
-    if tbl.Var1{i} == 'M' || tbl.Var1{i} == 'I'
-        data2.Var1{i} = 'MI';
+    if tbl.Sex{i} == 'M' || tbl.Sex{i} == 'I'
+        data2.Sex{i} = 'MI';
     end
     % Faz com que todos da classe I e F virem classe IF
-    if tbl.Var1{i} == 'I' || tbl.Var1{i} == 'F'
-        data3.Var1{i} = 'IF';
+    if tbl.Sex{i} == 'I' || tbl.Sex{i} == 'F'
+        data3.Sex{i} = 'IF';
     end
 end
 
@@ -36,24 +44,90 @@ data1 = [data1 samples];
 data2 = [data2 samples];
 data3 = [data3 samples];
 
-% Treinamento de cada classificador.
-mdl1 = fitcsvm(data1(6:N, :), 'Var1', 'KernelFunction', ...
-    'gaussian', 'KernelScale', 2.2, 'Standardize', true);
-mdl2 = fitcsvm(data2(6:N, :), 'Var1', 'KernelFunction', ...
-    'gaussian', 'KernelScale', 2.2, 'Standardize', true);
-mdl3 = fitcsvm(data3(6:N, :), 'Var1', 'KernelFunction', ...
-    'gaussian', 'KernelScale', 2.2, 'Standardize', true);
+%% K-Fold
+% Indices do K-Fold.
+kfold = crossvalind('Kfold', N, K);
+acertos = zeros(K, 1);
 
+predictedClass = [];
+realClass = [];
 
-% Predição da primeira amostra de cada classificador.
-[~, score1] = predict(mdl1, samples(1:5, :));
-[~, score2] = predict(mdl2, samples(1:5, :));
-[~, score3] = predict(mdl3, samples(1:5, :));
+for k = 1:K
+    %% Separa os indices de treino e teste.
+    teste = (kfold == k);
+    treino = ~teste;
+    
+    %% Treinamento de cada classificador.
+    mdl1 = fitcsvm(data1(treino, :), 'Sex', 'KernelFunction', ...
+        kernel, 'KernelScale', scale, 'Standardize', true);
+    mdl2 = fitcsvm(data2(treino, :), 'Sex', 'KernelFunction', ...
+        kernel, 'KernelScale', scale, 'Standardize', true);
+    mdl3 = fitcsvm(data3(treino, :), 'Sex', 'KernelFunction', ...
+        kernel, 'KernelScale', scale, 'Standardize', true);
 
-scores = array2table([score1 score2 score3], 'VariableNames', ...
-    [mdl1.ClassNames; mdl2.ClassNames; mdl3.ClassNames]);
+    %% Predição das amostras de teste para cada classificador.
+    [~, score1] = predict(mdl1, samples(teste, :));
+    [~, score2] = predict(mdl2, samples(teste, :));
+    [~, score3] = predict(mdl3, samples(teste, :));
 
-ml = mlclass(scores);
+    % Organização dos scores.
+    scores = array2table([score1 score2 score3], 'VariableNames', ...
+        [mdl1.ClassNames; mdl2.ClassNames; mdl3.ClassNames]);
 
-% CVMdl = crossval(mdl);
-% openExample('stats/TrainSupportVectorMachineRegressionModelExample');
+    % Classe mais provável.
+    ml = mlclass(scores);
+    
+    %% Cálculo da porcentagem de acerto da rodada.
+    acertos(k) = sum(ml == classes(teste))/sum(teste)*100;
+    fprintf("Acerto K=%2d: %2.2f%%\n", k, acertos(k));
+    
+    predictedClass = [predictedClass; ml];
+    realClass = [realClass; classes(teste)];
+end
+
+fprintf("Acerto Médio: %2.2f%%\n", mean(acertos));
+
+%% Gráficos
+% Confusion
+target = zeros(3, length(predictedClass));
+out = target;
+
+for i = 1:length(predictedClass)
+    if predictedClass(i) == 'M'
+        out(:,i) = [1 0 0]';
+    end
+    if predictedClass(i) == 'F'
+        out(:,i) = [0 1 0]';
+    end
+    if predictedClass(i) == 'I'
+        out(:,i) = [0 0 1]';
+    end
+
+    if realClass(i) == 'M'
+        target(:,i) = [1 0 0]';
+    end
+    if realClass(i) == 'F'
+        target(:,i) = [0 1 0]';
+    end
+    if realClass(i) == 'I'
+        target(:,i) = [0 0 1]';
+    end
+end
+
+plotconfusion(target, out);
+set(gca,'xticklabel',{'M' 'F' 'I' ''});
+set(gca,'yticklabel',{'M' 'F' 'I' ''});
+
+% Acertos por rodada
+figure;
+hold on;
+grid on;
+ylabel('Porcentagem de Acerto');
+xlabel('Valor de K');
+title('Porcentagem de Acertos K-Fold');
+set(legend('show'), 'Location', 'best');
+% ylim([0 100]);
+plot(acertos, 'DisplayName', 'Acertos', 'MarkerSize', 20, 'Marker', ...
+    '.', 'LineWidth', 2);
+plot(mean(acertos)*ones(1,K), 'DisplayName', 'Acerto Médio', ...
+    'LineStyle', '--', 'LineWidth', 2);
